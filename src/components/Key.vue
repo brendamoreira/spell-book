cb657f3c571a94b1d7fe3a5d82101e090b5bd244
Brenda Moreira <brendanatalia94@hotmail.com>
2020-06-02 14:09:45 +0200

    Basic Key component

---
+++ src/components/Key.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <form @submit.prevent="$emit('submit', apiKey)">
+            <input type="text" v-model="apiKey" placeholder="Type your key">
+            <button type="submit">Enter</button>
+        </form>
+    </div>
+</template>
+<script>
+export default {
+    name:'Key',
+    data() {
+        return{
+            apiKey: '',
+        }
+    },
+}
+</script>
+<style scoped>
+input{
+    font-family: 'Raleway', sans-serif;
+    font-size: 0.6rem;
+    width: 200px;
+    height: 16px;
+    margin: auto;
+    border: none;
+    border-radius: 2px;
+    opacity: 80%;
+}
+button{
+    margin: 2px;
+    border: none;
+    border-radius: 2px;
+    height: 16px;
+    opacity: 85%;
+}
+button:hover{
+    opacity: 100%;
+}
+</style>
